919e2042b4268df99f0281237c494a270234e91f
cc-smith <smitchr8@oregonstate.edu>
2021-07-27 15:38:31 -0600

    fixed filter function

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const axios = require('axios');
 const url = require('url');
 const fs = require('fs');
 const app = express();
@@ -10,60 +9,39 @@ let data = JSON.parse(fs.readFileSync('poses.json'));
 let poses = data["poses:"]
 
 app.get('/:query&:pose_name?&:category?&:difficulty?&:benefits?', (req, res) => {
-  var pose_filter = req.params
-  console.clear()
-  console.log("FILTER:", pose_filter)
+  // Get the filter criterion as provided in the get request url
+  var poseFilter = req.params
 
+  // Filter the poses.json file with the filter criterion
   var result = poses.filter(item => {
-    for (let key in pose_filter) {
+    var flag = true  // Set the conditional flag
+
+    for (let key in poseFilter) {
 
       // Skip first param and params that are undefined
-      if (key == "query" || pose_filter[key] == undefined) {
+      if (key == "query" || poseFilter[key] == undefined) {
         continue;
       }
 
-      console.log("\n\nkey:", key, "\nfilter:", pose_filter[key], "\nitem:", item[key])
+      // Convert the filter criteria to readable format
+      var poseFilterConverted = poseFilter[key].toLowerCase().split("-")
 
-      if (item[key] === undefined) {
-        return true;
-      }
+      // Search for each word within the filter
+      poseFilterConverted.forEach(function(word) {
+        if ( !item[key].toLowerCase().includes(word) ) {
+          flag = false  // word was not found in the json object
+        };
+      });
 
-      // if (item[key].toLowerCase().includes(pose_filter[key].toLowerCase())) {
-      //   return true;
-      // }
-      // if (item[key] === undefined || item[key] != pose_filter[key])
-      if (item[key] != pose_filter[key])
-        return false;
     }
-    return true;
+    return flag;
+
   });
 
-  res.send(result)
-})
-
-// axios.get('https://yoga-poses-api.herokuapp.com/search?', { 
-// app.get('http://localhost:5000/get?', {
-//   params: {
-//     pose: this.pose,
-//   }
-// })
-// .then(response => {
-//   console.log(params)
-//   // console.log(response.data.url);
-//   // console.log(response.data.explanation);
-// })
-// .catch(error => {
-//   console.log(error);
-// });
-
-
-// let testing = JSON.stringify(poses2)
-// fs.writeFile('myjsonfile.json', testing, function(err) {
-//   if (err) throw err;
-//   console.log('complete');
-//   }
-// );
+  // Send the filtered json to the user
+  res.header("Content-Type",'application/json');
+  res.send(JSON.stringify(result, null, 4));
+});
 
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
